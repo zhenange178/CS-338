74eeae8e34529219e6f4194de3c1c2cf84af1422
zhenange178 <zhenange178@gmail.com>
2024-05-27 00:05:01 -0400

    working API call feature

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
 <body>
 <h1>Welcome to the H&M Project</h1>
     <p>
-        <a href="initialize.php" class="button">Initialize database</a>
+        <a href="get_api.php" class="button">Fetch Data (may take long)</a> <br/>
+    </p>
+
+    <p>
+        <a href="initialize.php" class="button">Initialize database (Not working now)</a>
     </p>
 
     Other functions...

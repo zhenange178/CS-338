d4d113df46f8c0c39b82855b4790b9e84239c512
zhenange178 <zhenange178@gmail.com>
2024-06-14 18:42:58 -0400

    Revert "refactor folders in init"
    This reverts commit 254d3df983a43fe71a9e2ae48d7dc660e95fe749.

--- init/generate_sample.php
+++ init/generate_sample.php
@@ -1,3 +1,3 @@
-<?php include '../init/includes/development/generate_mock_data_sample.php'; ?>
-<?php include '../init/includes/development/create_database_sample.php'; ?>
-<?php include '../init/includes/development/populate_database_sample.php'; ?>
+<?php include '../init/includes/generate_mock_data_sample.php'; ?>
+<?php include '../init/includes/create_database_sample.php'; ?>
+<?php include '../init/includes/populate_database_sample.php'; ?>

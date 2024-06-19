287f172bb013815ddc42a40f25f814136735cd21
zhenange178 <zhenange178@gmail.com>
2024-06-19 18:55:05 -0400

    footer text

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
     </div>
     <div class="footer">
-        <p>&copy; 2024 [your names here?]. All rights reserved.</p>
+        <p>&copy; 2024 Angel Zheng et al. All rights reserved.</p>
     </div>
 </body>
 </html>

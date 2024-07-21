89cfb3ffdf9b9d6eeef9f4b7a06d8777dccf2707
zhenange178 <zhenange178@gmail.com>
2024-07-21 19:24:35 -0400

    new initialization options, change returned reasons

---
+++ init/database.php
@@ -0,0 +1,9 @@
+<div style="margin:10px 4px; font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;">
+<div>
+    <p><a href="/initialize.php">Back</a></p>
+</div>
+<h3>Reset Database</h3>
+<?php include '../init/includes/create_database.php'; ?>
+<?php include '../init/includes/populate_database_all.php'; ?>
+<?php include '../init/includes/redirect.php'; ?>
+</div>
--- initialize.php
+++ initialize.php
@@ -6,17 +6,17 @@
 </div>
 <div class="divider"></div>
 <div>
-    <h3>Initialize Everything</h3>
+    <h3>Production: Initialize Everything</h3>
     <b>Necessary for first-time users.</b> See below initialize everything, as mentioned above. <br />
     Mock data will be randomly generated and stored in your local "mock_data" JSON file. One will be created if it doesn't already exist. Please do not modify this file. Mock data include mock users, reviews, promo codes, and other non-product data. This, along with the real product data (see below), will be used to generate the database.<br/>
-    <br/><i>This will reset all data and database tables.</i>  
+    <br/><i>This will reset all production data and database tables. Product data will be refetched, and mock data will be randomly generated again. The database will be wiped and completely regenerated with this new data. Proceed with caution.</i>  
     <br/><br/>
     <a href="init/reset.php" class="initbutton buttonRed">Initialize everything</a>
     <br/><br/>
 </div>
 <div class="divider"></div>
 <div>
-    <h3>Update Product Data Only</h3>
+    <h3>Production: Update Product Data and Append to Existing Database</h3>
     Raw data is retrieved directly from the <a href="https://www2.hm.com/en_ca/index.html" target="_blank" rel="noopener noreferrer">H&M Canada site</a> using their public API.<br />
     A simlar fraction of the newest items in each of the selected sections are used.<br />
     The raw data is stored in your local "hm_product_list" JSON file. One will be created if it doesn't already exist. Please do not modify this file.<br />
@@ -39,8 +39,36 @@
 </div>
 <div class="divider"></div>
 <div>
-    <h3>Development: Initialize Sample</h3>
-    Same as the first full reset option above, but using smaller and static data files for development purposes.<br/>
+    <h3>Production: Reset Database</h3>
+    Select this option if changes were made to the JSON file and you would like to reflect them in the database, and/or you would like to reset all changes made to the database using any application features.<br/>Use the existing product data and mock data files to reset the production database. Data are stored in <code>init/data/hm_product_list.json</code> and <code>init/data/mock_data_list.json</code>. Make sure both files exist and contain data.<br/>All JSON data files will remain unchanged. All other changes to the database will be reset.
+    <br/><br/>
+    <?php
+    echo "Product list JSON: ";
+    $productFilePath = 'init/data/SAMPLE_hm_product_list.json';
+
+    if (file_exists($productFilePath)) {
+        echo '<a href="init/data/hm_product_list.json" class="button" target="_blank" rel="noopener noreferrer">view</a>';
+    } else {
+        echo '<i>file does not exist</i>';
+    }
+
+    echo "<br/>Mock data JSON: ";
+    $productFilePath = 'init/data/SAMPLE_mock_data.json';
+
+    if (file_exists($productFilePath)) {
+        echo '<a href="init/data/mock_data.json" class="button" target="_blank" rel="noopener noreferrer">view</a>';
+    } else {
+        echo '<i>file does not exist</i>';
+    }
+    ?>
+    <br/><br/>
+    <a href="init/database.php" class="initbutton buttonGreen">Reset Database</a> <!--update link-->
+    <br/><br/>
+</div>
+<div class="divider"></div>
+<div>
+    <h3>Development: Reset Sample Database</h3>
+    Same as the "Reset Database" option above, but using smaller and static data files for development purposes.<br/>
     Data are stored in <code>init/data/SAMPLE_hm_product_list.json</code> and <code>init/data/SAMPLE_mock_data_list.json</code>. Make sure both files exist and contain data.<br/>
     The <code>sampledatabase</code> database will be initialized and used to store this sample data, in the exact same format.
     <br/><br/>
@@ -64,7 +92,7 @@
     }
     ?>
     <br/><br/>
-    <a href="init/sample.php" class="initbutton buttonOrange">Initialize Sample Database</a>
+    <a href="init/sample.php" class="initbutton buttonOrange">Reset Sample Database</a>
     <br/><br/>
 </div>
 
